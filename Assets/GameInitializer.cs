8e1d5e47d1d6aaaec6bff7ecca74447999e4b4c8
Zayaan <72232563+grizzledwarvet23@users.noreply.github.com>
2025-12-22 21:41:44 -0600

    game initializer

---
+++ Assets/GameInitializer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class GameInitializer : MonoBehaviour
+{
+    [System.NonSerialized]
+    public bool isWorkshopMode = false;
+
+    void Start()
+    {
+        
+    }
+
+
+}
